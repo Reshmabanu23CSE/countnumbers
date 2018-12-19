4e4ec4d8ab8a23176aab0531e7d7b76d913c6d5d
Reshmabanu23 <36692753+Reshmabanu23CSE@users.noreply.github.com>
2018-12-19 14:14:28 +0530

    Update count.java

--- count.java
+++ count.java
@@ -5,14 +5,14 @@ public class Main {
 	
 	public static void main(String[] args) {
 		int Number, Count=0;
-	Scanner	sc = new Scanner(System.in);		
-	System.out.println("enter  number:");
+			Scanner	sc = new Scanner(System.in);		
+
 		Number = sc.nextInt();
 		
 		while(Number > 0) {
 			Number = Number / 10;
 			Count = Count + 1; 
 		}
-		System.out.format("\n Number of Digits in a Given Number = %d", Count);
-	}
+		System.out.println( Count);
+}
 }
